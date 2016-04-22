db1538e269011caa5a323e1fa8d15f37ddf45482
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-22 14:57:16 -0700

    solved most of string methods

--- drills.rb
+++ drills.rb
@@ -51,15 +51,27 @@ end
   # returns the first letter of the word
   # lowercases the first letter of the word
 
+def first_char(word)
+  word[0].downcase
+end
+
 #polly_wanna
   # takes in a word
   # echoes the original word
   # repeats the original word 3 times
   # returns a string with the word repeated
 
+def polly_wanna(word)
+  word * 3
+end
+
 #count_chars
   # takes in a word
   # returns the number of characters in the word
+
+def count_chars(word)
+  word.length
+end
 
 #yell
   # takes in a message
@@ -67,34 +79,54 @@ end
   # adds an exclamation point to the end of the message
   # returns the message
 
+def yell(message)
+  message.upcase + "!"
+end
+
 ## STRETCH ##
 #to_telegram
   # takes in a message
   # replaces periods with ' STOP'
   # returns the updated message
 
+def to_telegram(message)
+  message.gsub(/[.]/, ' STOP')
+end
+
 
 #spell_out
   # takes in a string
   # converts the string to lowercase
   # returns the input string, with characters seperated by dashes
-
+def spell_out(str)
+  str.downcase.split(//).join("-")
+end
 
 #seperate
   # takes in a string
   # seperates characters with a custom seperator, when supplied with one
   # seperates characters with dashes (by default)
   # returns the modified string
+def seperate(str, seperator="-")
+  str.downcase.split(//).join(seperator)
+end
 
 ## STRETCH ##
 #croon
   # seperates word characters with dashes
   # preserves whitespace between words
+  # google map
+def croon(str)
+  str.split(" ").map { |word| word.split(//).join("-")}.join(" ")
+end
 
 #palindrome_word?
   # determines whether a single word is a palindrome
   # ignores case
   # returns true or false
+def palindrome_word?(str)
+  str.downcase == str.reverse.downcase
+end
 
 
 ## SUPER STRETCH ##
@@ -110,12 +142,20 @@ end
   # ignores case
   # handles weird inputs gracefully
 
+
 #add_period
   # takes in a string
   # adds a period to the end of the sentence
   # does not add a period if one is already there
   # does not add a period if any form of terminal punctuation is present
   # returns the sentence
+  # googe include?
+  # better to use ternary statement here:
+  #{}"!?.".include?(str[-1]) ? str : str + "."
+def add_period(str)
+  return str if "!?.".include?(str[-1])
+  str + "."
+end
 
 
 ###########################
